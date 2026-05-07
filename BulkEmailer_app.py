6c3b2d43a531ffce576fce09f30c9c1bd7c20ae0
shashankshandilya-dotpe1754 <shashank.shandilya@dotpe.in>
2026-05-07 07:52:56 +0530

    Update BulkEmailer_app.py

--- BulkEmailer_app.py
+++ BulkEmailer_app.py
@@ -788,3 +788,61 @@ st.set_page_config(
     page_title="Bulk Email Sender",
     layout="centered"
 )
+
+
+# ---------------- CLEAR QUILL EMAIL BODY ---------------- #
+
+email_body = st.components.v1.html(
+    """
+    
+    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+    <div id="toolbar">
+
+        <button class="ql-bold"></button>
+        <button class="ql-italic"></button>
+        <button class="ql-underline"></button>
+
+        <button class="ql-list" value="ordered"></button>
+        <button class="ql-list" value="bullet"></button>
+
+        <button class="ql-link"></button>
+
+        <button class="ql-clean"></button>
+
+    </div>
+
+    <div id="editor" style="
+        background:white;
+        color:black;
+        height:320px;
+        font-size:14px;
+        font-family:Arial;
+        padding:10px;
+    ">
+    </div>
+
+    <script>
+
+        var quill = new Quill('#editor', {
+
+            theme: 'snow',
+
+            modules: {
+                toolbar: '#toolbar'
+            }
+
+        });
+
+        document.querySelector('.ql-editor').style.minHeight = "300px";
+        document.querySelector('.ql-editor').style.color = "black";
+        document.querySelector('.ql-editor').style.backgroundColor = "white";
+        document.querySelector('.ql-editor').style.caretColor = "black";
+
+    </script>
+
+    """,
+    height=420,
+    scrolling=True
+)
